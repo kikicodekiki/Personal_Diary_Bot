17c7e84204276847a4370414b58803201cabc73c
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-07 08:39:46 +0200

    find new API to use.. had to pay :( but it woooorks!!!

---
+++ secondAstrologyAPITest.py
@@ -0,0 +1,15 @@
+import http.client
+
+conn = http.client.HTTPSConnection("horoscope-astrology.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "<KEY>",
+    'x-rapidapi-host': "horoscope-astrology.p.rapidapi.com"
+}
+
+conn.request("GET", "/horoscope?day=today&sunsign=libra", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))

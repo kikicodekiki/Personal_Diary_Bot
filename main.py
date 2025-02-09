5bb0e611512eafb1c6bb6b6e99b139b082a50ed8
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-10 00:56:19 +0200

    start implementing the different astrology commands. Maybe I should implement it with the command pattern later on...

--- main.py
+++ main.py
@@ -35,6 +35,42 @@ class TelegramBot:
                 self.log_period(message)
 
     def ask_zodiac(self, message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        # display each zodiac sign so that the user can choose theirs
+        for sign in RapidAPIHoroscope.signs:
+            markup.add(types.KeyboardButton(text = sign))
+        self.bot.send_message(message.chat.id, "Please, select your zodiac sign: ", reply_markup=markup)
+        self.bot.register_next_step_handler(message.chat.id, self.choose_horoscope_command)
+
+    def choose_horoscope_command(self, message):
+        """Showcases the different functionalities, related to astrology, that the bot supports."""
+        zodiac_sign = message.text.lower()
+        user = message.from_user.username
+        self.db.update_user(user, "zodiac_sign", zodiac_sign)
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton(text='Get Horoscope for Today'))
+        markup.add(types.KeyboardButton(text='Check Compatability'))
+        markup.add(types.KeyboardButton(text='Numerology Reading'))
+        self.bot.send_message(message.chat.id, "Please, choose a command: ", reply_markup=markup)
+        self.bot.register_next_step_handler(message, self.handle_horoscope_command, zodiac_sign)
+
+    def handle_horoscope_command(self, message, zodiac_sign):
+        """Handles each command that the user may have picked."""
+        if message.text == 'Get Horoscope for Today':
+            """Give out the person's horoscope."""
+            instance = RapidAPIHoroscope(sign=zodiac_sign)
+            horoscope = instance.get_horoscope()
+            self.bot.send_message(message.chat.id, f"Your horoscope for today:\n\n {horoscope}\n")
+        elif message.text == 'Check Compatability':
+            """Ask for the other person's zodiac sign.'"""
+            self.ask_compatability_sign(message, zodiac_sign)
+        elif message.text == 'Numerology Reading':
+            self.ask_numerology_number(message)
+
+    def ask_compatability_sign(self, message, zodiac_sign):
+        pass
+
+    def ask_numerology_number(self, message):
         pass
 
     def log_mood(self, message):

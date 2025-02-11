d0ba96a2ad96105ff998067c806ec82c151ddb10
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-11 23:39:28 +0200

    Start implementing the Command Design Pattern to ensure good OOP design and better encapsulation bcz the TelegramBot class was managing too many things that he shouldn't be caring about.

---
+++ commands/base.py
@@ -0,0 +1,9 @@
+"""Defines the interface for the Command Design Pattern."""
+from abc import ABC, abstractmethod # import needed modules for the creation of an abstract class
+
+
+class Command(ABC):
+    """Abstract base class for all bot commands."""
+    @abstractmethod
+    def execute(self):
+        pass

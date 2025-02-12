911347c2ac775b5db8689e3df9d509a2f291c5e1
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-12 08:28:32 +0200

    Implement the other commands from the astrological part of the command design pattern

--- commands/GetDailyHoroscopeCommand.py
+++ commands/GetDailyHoroscopeCommand.py
@@ -1,4 +1,13 @@
-from base import Command
+from commands.base import Command
+from telebot import types
+from RapidAPIHoroscope import RapidAPIHoroscope
+
 
 class GetDailyHoroscopeCommand(Command):
-    pass
+    """Handles the retrieval of the daily horoscope."""
+
+    def execute(self, bot, db, message, zodiac_sign):
+        """Fetches the daily horoscope."""
+        instance = RapidAPIHoroscope(zodiac_sign)
+        horoscope = instance.get_horoscope()
+        bot.send_message(message.chat.id, f"Your horoscope for today:\n\n{horoscope}")
--- commands/GetNumerologyCommand.py
+++ commands/GetNumerologyCommand.py
@@ -1,4 +1,23 @@
-from base import Command
+from commands.base import Command
+from telebot import types
+from RapidAPIHoroscope import RapidAPIHoroscope
 
 class GetNumerologyCommand(Command):
-    pass
+    """Handles numerology readings."""
+    def execute(self, bot, db, message, zodiac_sign):
+        """Asks for the person's life path number."""
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for i in range (1,10):
+            markup.add(types.KeyboardButton(text=str(i)))
+        bot.send_message(message.chat.id,
+                         "Please select your life path number.\n"
+                         "You can find it by adding up all the digits from your birth date until you get a single digit number.\n"
+                         )
+        bot.register_next_step_hander(message, self.get_numerology, bot)
+
+    def get_numerology(self, message, bot):
+        """Fetches and sends the numerology results."""
+        numerology_number = int(message.text)
+        instance = RapidAPIHoroscope(numerology_number=numerology_number)
+        numerology = instance.get_numerology()
+        bot.send_message(message.chat.id, f"Your life path reading:\n\n{numerology}")

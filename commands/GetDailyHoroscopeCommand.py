887bd29ab397958736cd4649c74c3039d3ff290b
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-12 23:03:56 +0200

    Finish everything surrounding the Astrological part of the bot => next up: periiod tracking!

--- commands/GetDailyHoroscopeCommand.py
+++ commands/GetDailyHoroscopeCommand.py
@@ -11,3 +11,10 @@ class GetDailyHoroscopeCommand(Command):
         instance = RapidAPIHoroscope(sign=zodiac_sign)
         horoscope = instance.get_horoscope()
         bot.send_message(message.chat.id, f"Your horoscope for today:\n\n{horoscope}")
+        self.return_to_main_menu(bot, message)
+
+    def return_to_main_menu(self, bot,message):
+        """Returns to the main menu."""
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
+        markup.add(types.KeyboardButton(text="Get Astrology Reading"))
+        bot.send_message(message.chat.id, "Would you like to do something else?", reply_markup=markup)

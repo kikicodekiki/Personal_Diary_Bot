cae6bf1241f6684a76ab4627dcf05fa20913b1fd
kikicodekiki <ekaterinastoyanova999@gmail.com>
2025-02-13 00:03:09 +0200

    Separate the database table into two (read online that it's better for them to continue being in the same SQL database file): 1. users+their zodiac_signs 2. menstrual cycle logs

--- Database.py
+++ Database.py
@@ -8,19 +8,28 @@ class Database:
         self.connection = sqlite3.connect(database_name, check_same_thread=False)
         # check_same_thread = False => the connection may be accessed in multiple threads;
         self.cursor = self.connection.cursor()
-        self.create_table()
+        self.create_tables()
 
-    def create_table(self):
-        self.cursor.execute('''
+    def create_tables(self):
+        """Creates two tables =>
+            one that keeps users and their zodiac signs and
+            another that keeps their menstrual cycle logs."""
+        self.cursor.execute("""
                     CREATE TABLE IF NOT EXISTS users (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         username TEXT UNIQUE,
                         zodiac_sign TEXT,
-                        period_start DATE,
-                        period_end DATE,
-                        mood TEXT
-                    )
-                ''')
+                    );
+                """) # table for the users and their zodiac signs -> keeping this so as to not refactor code
+        # create the table for the menstrual cycle
+        self.cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS menstrual_logs (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            user_id INTEGER NOT NULL,
+                            start_date TEXT NOT NULL,
+                            FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+                            );
+        """) # make sure that when a user gets deleted => their data also gets deleted
         self.connection.commit()
 
     def add_user(self, username):
